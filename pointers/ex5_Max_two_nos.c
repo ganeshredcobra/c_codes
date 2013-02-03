da749bcea9107e6be29955a25ea7ba2eac651900
Ganesh <ganeshredcobra@gmail.com>
2013-02-03 13:01:43 +0530

    Find Max of two No's

---
+++ pointers/ex5_Max_two_nos.c
@@ -0,0 +1,37 @@
+//      
+//      Copyright 2013 Ganesh <ganeshredcobra@gmail.com>
+//      
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+
+#include <stdio.h>
+
+int largest(int a,int b)
+{
+	if(a>b)
+		return a;
+	else
+		return b;
+}
+
+void main()
+{
+	int x,y,big;
+	printf("Enter the values of x and y \n");
+	scanf("%d%d",&x,&y);
+	big=largest(x,y);
+	printf("Maximum(%d,%d)=%d \n",x,y,big);
+}
