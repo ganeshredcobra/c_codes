4007852ebe552bc17d9df8b97f83f3f3fdd7188d
Ganesh <ganeshredcobra@gmail.com>
2013-02-20 17:44:41 +0530

    Multiply fractions using structure

---
+++ struct/ex1_frac_multply.c
@@ -0,0 +1,38 @@
+//      
+//      Copyright 2013 Ganesh <ganeshredcobra@gmail.com>
+//      
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+
+#include <stdio.h>
+
+typedef struct
+{
+	int numerator;
+	int denominator;
+}FRACTION;
+
+void main()
+{
+	FRACTION a,b,c;
+	printf("Enter fraction1 in the form of x/y:");
+	scanf("%d/%d",&a.numerator,&a.denominator);
+	printf("Enter fraction2 in the form of x/y:");
+	scanf("%d/%d",&b.numerator,&b.denominator);
+	c.numerator=a.numerator * b.numerator;
+	c.denominator=a.denominator * b.denominator;
+	printf("%d/%d * %d/%d=%d/%d \n",a.numerator,a.denominator,b.numerator,b.denominator,c.numerator,c.denominator);
+}
