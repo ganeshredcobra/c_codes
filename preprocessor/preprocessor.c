1b979a603b3e5798b10c2f266b746ba356aa64b9
Ganesh <ganeshredcobra@gmail.com>
2013-05-28 18:42:32 +0530

    Preprocessor directives

---
+++ preprocessor/preprocessor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAX_ARRAY_LENGTH 20
+#undef FILESIZE
+#define FILESIZE 42
+
+#ifndef MESSAGE
+   #define MESSAGE "You Wish!"
+#endif
+
+#ifdef DEBUG
+ //hello
+#endif
+
+void main()
+{
+	printf("File :%s\n",__FILE__);
+	printf("Date :%s\n",__DATE__);
+	printf("Time :%s\n",__TIME__);
+	printf("Line :%d\n",__LINE__);
+	printf("Ansi :%d\n",__STDC__);
+	printf("Max_Array_len :%d\n",MAX_ARRAY_LENGTH);
+	printf("FILESIZE :%d\n",FILESIZE);
+	printf("Message :%s\n",MESSAGE);
+}
