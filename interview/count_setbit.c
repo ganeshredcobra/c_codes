9aed040d08a771bf52f904926c31e563069e3b75
ganeshredcobra <ganeshredcobra@gmail.com>
2016-02-27 15:18:12 +0530

    Count Set bits

---
+++ interview/count_setbit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,count=0;
+    unsigned int BIT;
+    unsigned char CL= 0x1F;	//Clear
+
+    for(i=0; i<0xF; i++)
+    {
+        BIT=CL&(1<<i);
+        if(BIT)
+        {
+            count++;
+
+        }
+
+    }
+    printf("No of set Bits %d\n",count);
+    return 0;
+}
